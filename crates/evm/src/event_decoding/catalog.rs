07ba31d1e4a30e8bfdc87ae9e3a96d43ca0a9242
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2026-08-13 16:38:14 +0100

    Revert "chore: tests"
    This reverts commit b9751261e480673bcb4dd1107fb19e370417d4fc.

--- crates/evm/src/event_decoding/catalog.rs
+++ crates/evm/src/event_decoding/catalog.rs
@@ -171,11 +171,6 @@ const BONDING_REGISTRY: &[EvmEventDefinition] = &[
         None,
     ),
     EvmEventDefinition::new(
-        "BondedCheckpointsSet",
-        "BondedCheckpointsSet(address)",
-        None,
-    ),
-    EvmEventDefinition::new(
         "BondingAssetConfigUpdated",
         "BondingAssetConfigUpdated(address,address,uint256,uint256,uint8,uint8,uint64)",
         None,
